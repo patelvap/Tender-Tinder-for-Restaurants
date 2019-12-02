f89cfff5f7b8d95ed6d8349e7735e797b830b067
Vraj Patel <patelvap@gmail.com>
2019-12-02 17:33:51 -0500

    add logo and app skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 const fetch = require('node-fetch');
 
-function App() {
+class App extends Component {
   constructor() {
     super();
     this.state = {
@@ -19,10 +19,10 @@ function App() {
     return (
       <Router basename = { process.env.PUBLIC_URL }>
         <div className="App">
-          
+
         </div>
       </Router>
-    );
+    )
   }
 }
 
