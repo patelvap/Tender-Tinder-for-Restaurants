0b14bd485a5acdbce3a4def14267f65c9bca35f2
Vraj Patel <patelvap@gmail.com>
2019-12-02 17:11:44 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+const fetch = require('node-fetch');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  getParams = () => {
+
+  }
+
+  render() {
+    return (
+      <Router basename = { process.env.PUBLIC_URL }>
+        <div className="App">
+          
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
