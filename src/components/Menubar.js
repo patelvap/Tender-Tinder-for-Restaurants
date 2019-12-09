03bcfadd149b2021398c3eaca03b3a5b8e644bd8
Vraj Patel <patelvap@gmail.com>
2019-12-09 14:49:12 -0500

    rebase pull

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -4,76 +4,40 @@ import {Navbar, NavbarBrand, NavbarItem, Icon, NavbarBurger, NavbarMenu, NavbarS
 //import './Menubar.css'
 
 
-
-export default class MenuBar extends React.Component{
-    constructor() {
-        super()
-    }
-
-    render() {
-        return (
-            <Navbar style={{ border: 'solid 1px #00D1B2', margin: '0' }}>
-            <NavbarItem>
-            <NavbarLink>
-            <Button isColor='blue' onClick={this.props.loginPopup}>Login</Button>
-                </NavbarLink>
-            </NavbarItem>
-            <NavbarItem>
-            <NavbarLink>
-            <a href="#news">News</a>
-          
-                </NavbarLink>
-            </NavbarItem>
-            <NavbarItem>
-            <NavbarLink>
-            <a href="#contact">Contact</a>
-          
-                </NavbarLink>
-            </NavbarItem>
-            <NavbarItem>
-            <NavbarLink>
-            <a href="#about">About</a>
-                </NavbarLink>
-            </NavbarItem>
-          </Navbar>
+const MenuBar = () => {
+    return (
+        
+        /*<div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+        </div> */
+        <Navbar style={{ border: 'solid 1px #00D1B2', margin: '0' }}>
+        <NavbarItem>
+        <NavbarLink>
+        <Button isColor='blue' >Login</Button>
+        </NavbarLink>
+        </NavbarItem>
+        <NavbarItem>
+        <NavbarLink>
+        <a href="#news">News</a>
+        
+        </NavbarLink>
+        </NavbarItem>
+        <NavbarItem>
+        <NavbarLink>
+        <a href="#contact">Contact</a>
+        
+        </NavbarLink>
+        </NavbarItem>
+        <NavbarItem>
+        <NavbarLink>
+        <a href="#about">About</a>
+        </NavbarLink>
+        </NavbarItem>
+        </Navbar>
         );
     }
-}
-
-// const MenuBar = () => {
-//     return (
-        
-// /*<div class="topnav">
-//   <a class="active" href="#home">Home</a>
-//   <a href="#news">News</a>
-//   <a href="#contact">Contact</a>
-//   <a href="#about">About</a>
-// </div> */
-// <Navbar style={{ border: 'solid 1px #00D1B2', margin: '0' }}>
-//   <NavbarItem>
-//   <NavbarLink>
-//   <Button isColor='blue' onClick={loginPopup}>Login</Button>
-//       </NavbarLink>
-//   </NavbarItem>
-//   <NavbarItem>
-//   <NavbarLink>
-//   <a href="#news">News</a>
-
-//       </NavbarLink>
-//   </NavbarItem>
-//   <NavbarItem>
-//   <NavbarLink>
-//   <a href="#contact">Contact</a>
-
-//       </NavbarLink>
-//   </NavbarItem>
-//   <NavbarItem>
-//   <NavbarLink>
-//   <a href="#about">About</a>
-//       </NavbarLink>
-//   </NavbarItem>
-// </Navbar>
-//     );
-// }
-
-// export default MenuBar;
+    
+    export default MenuBar;
