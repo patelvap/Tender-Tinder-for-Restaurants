5ef9626f8daeac4accfef227f3f9337f42b78927
Sunisc <surya.poddutoori@gmail.com>
2019-12-11 16:24:20 -0500

    fixed the review system not working. follow the same format to avoid scoping issues

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,52 +1,79 @@
-import React, { Component } from 'react';
-import { Column, Content, Title, View, TextArea, Text, Box, Field, Label, Control, Input, Button } from 'bloomer';
+import React, { Component } from "react";
+import {
+  Column,
+  Content,
+  Title,
+  View,
+  TextArea,
+  Text,
+  Box,
+  Field,
+  Label,
+  Control,
+  Input,
+  Button
+} from "bloomer";
 import CommentCard from "./CommentCard";
-import axios from 'axios';
-
+import axios from "axios";
 
 export default class Review extends Component {
-    constructor() {
-        super();
-        const pubRoot = new axios.create({
-            baseURL: "http://localhost:3000/public"
-          });
-        
+  constructor() {
+    super();
+    const pubRoot = new axios.create({
+      baseURL: "http://localhost:3000/public"
+    });
 
-        this.data = ""
-        this.state = {
-            review : "", //(required)
-            username : "tenderboys", //(required)
-            events:"ffefe"
-        }
+    this.data = "";
+    this.state = {
+      review: "", //(required)
+      username: "tenderboys", //(required)
+      events: "ffefe",
+      users: [],
+      reviews: [],
+      reload: 0
     };
+  }
 
-    getData() {
-        const pubRoot = new axios.create({
-            baseURL: "http://localhost:3000/public"
-          });
-        var strr = [];
-         pubRoot.get('/reviews')
-        .then(function (response) {
-        //    console.log("got the data");
-           //console.log(response.data.result)
-           strr.push(response)
-          })
-          .catch(function (error) {
-        });
-        return strr
+  getData() {
+    const pubRoot = new axios.create({
+      baseURL: "http://localhost:3000/public"
+    });
+    // var strr = [];
+    let reviews = [];
+    let users = [];
+    pubRoot
+      .get("/reviews")
+      .then(response => {
+        users.push(response.data.result.username);
+        reviews.push(response.data.result.review);
+        this.updateBoxes(users, reviews).bind(this);
+        // strr.push(response);
+      })
+      .catch(function(error) {});
+  }
+
+  updateBoxes(u, r) {
+    this.setState({
+      users: u,
+      reviews: r
+    });
+  }
+
+  render() {
+    if (this.state.reload < 1) {
+      this.getData();
+      this.setState({
+        reload: 1
+      });
     }
 
-    render() {    
-      console.log(this.getData())
-        return (
-            <Box>
-            <Column isSize="1/2">
-                <Content>
-                    
-            {this.state.events}
-          </Content>
-          </Column>
-        </Box>
-        )
-    }
+    return (
+      <Box>
+        <Column isSize="1/2">
+          <Content>{this.state.users[0]}</Content>
+          <Content>{this.state.reviews[0]}</Content>
+        </Column>
+      </Box>
+    );
+  }
 }
