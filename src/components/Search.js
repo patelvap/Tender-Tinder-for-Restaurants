eb30f06d10b447d1575487d33e052b8e3ba74b3a
Sunisc <surya.poddutoori@gmail.com>
2019-12-10 21:36:52 -0500

    heavily upgraded filtering

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,79 +1,180 @@
-import React, { Component } from 'react';
-import { Box, Field, Label, Control, Input, Button } from 'bloomer';
+import React, { Component } from "react";
+import {
+  Box,
+  Field,
+  Label,
+  Control,
+  Input,
+  Button,
+  Panel,
+  PanelHeading,
+  Column,
+  Columns,
+  Modal,
+  ModalClose,
+  ModalCard,
+  Container,
+  ModalCardHeader,
+  ModalCardBody,
+  Delete,
+  ModalCardFooter,
+  ModalCardTitle,
+  Card
+} from "bloomer";
+import { PanelBlock } from "bloomer/lib/components/Panel/PanelBlock";
+import { ModalBackground } from "bloomer/lib/components/Modal/ModalBackground";
+import { ModalContent } from "bloomer/lib/components/Modal/ModalContent";
 
 export default class Search extends Component {
-    constructor() {
-        super();
-        this.state = {
-            latitude: '', //(required)
-            longitude: '', //(required)
-            categories: '', //delimited strong of categories (optional)
-            radius: '', //(optional)
-            term: '', //(optional)
-            offset: '', //(optional)
-            limit : '', //(optional)
-        }
-        navigator.geolocation.getCurrentPosition((position => {
-            this.setState({
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-            });
-        }))
-    }
+  constructor() {
+    super();
+    this.state = {
+      latitude: "", //(required)
+      longitude: "", //(required)
+      categories: "", //delimited strong of categories (optional)
+      radius: "", //(optional)
+      term: "", //(optional)
+      offset: "", //(optional)
+      limit: "", //(optional)
+      isActive: false
+    };
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+    });
+  }
 
-    updateSearch = (e) => {
-        e.preventDefault();
-        this.setState({
-            [e.target.name]: e.target.value
-        });
-    }
+  updateSearch = e => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    submit = (e) => {
-        e.preventDefault();
-        navigator.geolocation.getCurrentPosition((position => {
-            this.setState({
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-            });
-        }))
-        // this.props.history.push(`latitude=${this.state.latitude}&
-        //                         longitude=${this.state.longitude}&
-        //                         categories=${this.state.categories}&
-        //                         radius=${this.state.radius}&
-        //                         term=${this.state.term}&
-        //                         offset=${this.state.offset}&
-        //                         limit=${this.state.limit}`);
-        const {latitude, longitude, categories, radius, term, offset, limit } = this.state;
-        this.props.getTargets(latitude, longitude, categories, radius, term, offset, limit);
-    }
-//need to add categories in this - combo box
-    render() {
-        return (
-            <Box>
-                <form onSubmit={this.submit}>
-                    <Field isHorizontal>
-                        <Label>Search Term</Label>
-                        <Control>
-                            <Input name="term" type="text" placeholder="Seach Term" onChange={this.updateSearch}></Input>
-                        </Control>
-                        <Label>Search Radius</Label>
-                        <Control>
-                            <Input name="radius" type="text" placeholder="Radius" onChange={this.updateSearch}></Input>
-                        </Control>
-                        <Label>Search Term</Label>
-                        <Control>
-                            <Input name="term" type="text" placeholder="Seach Term" onChange={this.updateSearch}></Input>
-                        </Control>
-                        <Label>Result Limit</Label>
-                        <Control>
-                            <Input name="limit" type="text" placeholder="Limit" onChange={this.updateSearch}></Input>
-                        </Control>
-                    </Field>
-                    <Field>
-                        <Button isColor='primary' type="submit">Search</Button>
-                    </Field>
-                </form>
-            </Box>
-        )
-    }
+  submit = e => {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+    });
+    // this.props.history.push(`latitude=${this.state.latitude}&
+    //                         longitude=${this.state.longitude}&
+    //                         categories=${this.state.categories}&
+    //                         radius=${this.state.radius}&
+    //                         term=${this.state.term}&
+    //                         offset=${this.state.offset}&
+    //                         limit=${this.state.limit}`);
+    const {
+      latitude,
+      longitude,
+      categories,
+      radius,
+      term,
+      offset,
+      limit
+    } = this.state;
+    this.props.getTargets(
+      latitude,
+      longitude,
+      categories,
+      radius,
+      term,
+      offset,
+      limit
+    );
+  };
+
+  changeActive() {
+    this.setState({
+      isActive: !this.state.isActive
+    });
+  }
+
+  //need to add categories in this - combo box
+  render() {
+    return (
+      <Container>
+        <Button onClick={this.changeActive.bind(this)}>
+          Choose your filter!
+        </Button>
+        <form onSubmit={this.submit}>
+          <Modal isActive={this.state.isActive}>
+            <ModalBackground />
+            <ModalCard>
+              <ModalCardHeader>
+                <ModalCardTitle>Filters!</ModalCardTitle>
+              </ModalCardHeader>
+              <ModalCardBody>
+                <Panel>
+                  <PanelBlock>
+                    <Label>Search Term</Label>
+                    <Control>
+                      <Input
+                        name="term"
+                        type="text"
+                        placeholder="Enter restaurant"
+                        onChange={this.updateSearch}
+                      ></Input>
+                    </Control>
+                  </PanelBlock>
+                  <PanelBlock>
+                    <Label>Search Radius</Label>
+                    <Control>
+                      <Input
+                        name="radius"
+                        type="text"
+                        placeholder="Radius"
+                        onChange={this.updateSearch}
+                      ></Input>
+                    </Control>
+                  </PanelBlock>
+                  <PanelBlock>
+                    <Label>Offset Limit</Label>
+                    <Control>
+                      <Input
+                        name="offset"
+                        type="text"
+                        placeholder="Offset the list of businesses"
+                        onChange={this.updateSearch}
+                      ></Input>
+                    </Control>
+                  </PanelBlock>
+                  <PanelBlock>
+                    <Label>Result Limit</Label>
+                    <Control>
+                      <Input
+                        name="limit"
+                        type="text"
+                        placeholder="Limit"
+                        onChange={this.updateSearch}
+                      ></Input>
+                    </Control>
+                  </PanelBlock>
+                </Panel>
+              </ModalCardBody>
+              <ModalCardFooter>
+                <Button
+                  isColor="primary"
+                  type="submit"
+                  onClick={this.changeActive.bind(this)}
+                >
+                  Search
+                </Button>
+                <Button
+                  isColor="warning"
+                  onClick={this.changeActive.bind(this)}
+                >
+                  Cancel
+                </Button>
+              </ModalCardFooter>
+            </ModalCard>
+          </Modal>
+        </form>
+      </Container>
+    );
+  }
 }
