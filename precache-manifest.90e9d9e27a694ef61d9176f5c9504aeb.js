e5fd615e1593d2712cd1c6855fe62325a46fb10e
Vraj Patel <patelvap@gmail.com>
2019-12-12 15:09:48 -0500

    Updates

---
+++ precache-manifest.90e9d9e27a694ef61d9176f5c9504aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d72bc1f0fcfc1a52e3a3d938409c4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e781231058cc39099e62",
+    "url": "/static/css/main.5f6323c7.chunk.css"
+  },
+  {
+    "revision": "0cad4fa6090276d41507",
+    "url": "/static/js/2.7b950365.chunk.js"
+  },
+  {
+    "revision": "fe3316f83d32803e44294fa7bd41b01b",
+    "url": "/static/js/2.7b950365.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e781231058cc39099e62",
+    "url": "/static/js/main.f752d93b.chunk.js"
+  },
+  {
+    "revision": "b370d900e2e38b8e9000",
+    "url": "/static/js/runtime-main.5353a6de.js"
+  }
+]);
