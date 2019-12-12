1bccf46882b9b8a5445575fb357abea3023c5538
elwyn578 <42598683+elwyn578@users.noreply.github.com>
2019-12-12 12:53:10 -0500

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -9,57 +9,6 @@ export default class Welcome extends Component {
       comb: [],
       login: ""
     };
-<<<<<<< HEAD
-    
-    getData() {
-        const pubRoot = new axios.create({
-            baseURL: "http://localhost:3000/public"
-        });
-        
-        let comb = this.state.comb;
-        pubRoot
-        .get("/users")
-        .then(response => {
-            comb.push(response.data.result.length)
-            this.updateBoxes(comb);
-        })
-        .catch(function (error) { });
-    }
-    
-    updateBoxes(c) {
-        this.setState({
-            comb: c
-        });
-    }
-    
-    render() {  
-        if(this.state.comb[0]===0){
-            return(
-                <Box>
-                    <Title>
-                        We currently have {this.state.comb[0]} hungry people like you using our site. Be the first to get top insight to best local foods
-                    </Title>
-                    <h1>Sign up to be the First User on the Site!</h1>
-                </Box>
-            )
-        }
-        
-        return (
-            <Box>
-                <Title>
-                    Welcome to Tender
-                </Title>
-                <h4>Join the more than {this.state.comb[0]} users currently finding great food on our site!</h4>
-                <br></br>
-                <br></br>
-                <p>
-                    From the producers of TINDER (jk), comes the new restaurants matching app. Get Paired with the best local restaurants with a click of a button.
-                    Use the search tab above to start looking for restaurants near your location. Make sure to turn on location services for a pleasurable experience. Also feel free to post restaurants suggestions or reviews for us under the Reviews Tab
-                </p>
-            </Box>
-            )
-        }
-=======
     this.getData();
   }
 
@@ -95,7 +44,6 @@ export default class Welcome extends Component {
           <h1>Sign up to be the First User on the Site!</h1>
         </Box>
       );
->>>>>>> 20e746d8b04e1a8c8ceef2ef1b64d89c235f4f12
     }
 
     return (
